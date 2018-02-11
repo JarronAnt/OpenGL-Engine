01a243a58fe21c0b50da24afb3a1c62b5f6cdaca
SSPYR0 <jarronblizzard@gmail.com>
2018-02-10 20:00:28 -0500

    change file extensions
    switched to common shader extension convention i.e .vert and .frag files

--- src/shaders/TerrainShader.java
+++ src/shaders/TerrainShader.java
@@ -9,8 +9,8 @@ import entities.Light;
 public class TerrainShader extends ShaderProgram{
 	
 	
-	private static final String VERTEX_FILE = "src/shaders/terrainVertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/shaders/terrainFragmentShader.txt";
+	private static final String VERTEX_FILE = "src/shaders/terrainVertexShader.vert";
+	private static final String FRAGMENT_FILE = "src/shaders/terrainFragmentShader.frag";
 	
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
