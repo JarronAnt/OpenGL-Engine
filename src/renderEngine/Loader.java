73f66cf449a927ae0c3edacd722b5df54d6c6806
SSPYR0 <jarronblizzard@gmail.com>
2018-02-11 11:23:10 -0500

    MipMapping
    added mipmapping code

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -42,7 +42,13 @@ public class Loader {
 		Texture texture = null;
 		try {
 			texture = TextureLoader.getTexture("PNG",
-					new FileInputStream("res/" + fileName + ".png"));
+				new FileInputStream("res/" + fileName + ".png"));
+			//MipMapping Code
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			//change the -1 depending on how aggresive u want mipmapping
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -1);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("Tried to load texture " + fileName + ".png , didn't work");
